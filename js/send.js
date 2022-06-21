26d804a04c178da7e9e514dfbb7f6842239d3c0b
agrski06 <agrski06@gmail.com>
2022-06-21 18:19:28 +0200

    added some info

--- js/send.js
+++ js/send.js
@@ -8,3 +8,56 @@ function sendMessage() {
         console.log("Request complete! response:", res);
       });
 }
+
+async function getInfo() {
+  let url = ''
+  let name = ''
+  let status = ''
+
+  await fetch("http://localhost:8080/api/bot-avatar")
+    .then(function(response) {
+      return response.text();
+    }).then(function(data) {
+      url = data;
+    });
+
+  await fetch("http://localhost:8080/api/bot-name")
+    .then(function(response) {
+      return response.text();
+    }).then(function(data) {
+      name = data;
+    });
+
+  await fetch("http://localhost:8080/api/bot-status")
+    .then(function(response) {
+      return response.text();
+    }).then(function(data) {
+      status = data;
+    });
+    console.log(status)
+  document.getElementById("avatar").innerHTML = '<img src="' + url + '" alt="Avatar">'
+  document.getElementById("name").innerHTML = name
+  document.getElementById("status").innerHTML = status
+  document.getElementById("status").style = status == 'CONNECTED' ? 'color: green;' : 'color: red';
+
+}
+
+
+window.addEventListener('load', function () {
+  getInfo();
+})
+
+
+// timeout request todo???
+/*
+const controller = new AbortController();
+
+const timeoutId = setTimeout(() => controller.abort(), 5000);
+
+const req = async () => {
+  const response = await fetch(url, { signal: controller.signal });
+  //...
+  clearTimeout(timeoutId);
+};
+req();
+*/
